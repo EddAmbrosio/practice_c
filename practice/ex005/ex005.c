92a73e678c62c1f9b52871194c0e1cd246b3f5e4
Edmilson Santos de Almeida <99562810+EddAmbrosio@users.noreply.github.com>
2022-09-12 23:11:09 -0300

    update

---
+++ practice/ex005/ex005.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    /*int i = 55;
+    char a, b;
+
+    a = "1";
+    b = "2";
+
+    printf("%i", i);
+    
+   1º every variable must have a name
+   2º every variable must have a type
+   3º every variable must have a size
+   4º every variable must have a value
+   
+   */
+
+    return 0;
+}
